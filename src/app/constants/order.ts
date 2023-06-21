d95798d23161e4b80ecec450afc210ea1cd00d15
Andrii Khoma <anik123wb@gmail.com>
2023-06-21 23:14:52 +0300

    additional order statuses

--- src/app/constants/order.ts
+++ src/app/constants/order.ts
@@ -2,6 +2,10 @@ import { ORDER_STATUS } from '../../shop-shared/constants/order';
 
 export const STATUS_TO_SEVERITY_MAP = {
   [ORDER_STATUS.CREATED]: 'primary',
+
+  [ORDER_STATUS.PENDING]: 'primary',
+  [ORDER_STATUS.PAID]: 'info',
+
   [ORDER_STATUS.FAILED]: 'danger',
   [ORDER_STATUS.FINISHED]: 'success',
 };
